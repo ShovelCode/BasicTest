960bc3e19fb1dcd68f4ee996bc9e10c279d31a68
Christopher Ray <vde435@gmail.com>
2021-06-27 13:19:35 -0500

    bad comments

--- src/QuickTester.java
+++ src/QuickTester.java
@@ -1,6 +1,21 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class QuickTester {
-    public static void main(String[]args){
+    public static void main(String[]args) throws SQLException, ClassNotFoundException {
+
         System.out.println("testing");
-        //put a PreparedStatement here.
+        //Connection con = new Connection();
+        /*
+        Map map = con.getTypeMap();
+        map.put("mySchemaName.EMPLOYEES",
+                Class.forName("Employees"));
+        con.setTypeMap(map);
+         */
+      //  PreparedStatement pstmt = con.prepareStatement("UPDATE EMPLOYEES SET SALARY = ? WHERE ID = ?");
+       // System.out.println(pstmt.toString());
+
+
     }
 }
